59159d541b89b1d1b7157c56c7b4a259fa046119
Diego Tejada <stanleytejadaj@gmail.com>
2024-08-29 22:02:00 -0700

    Setting up Database and Login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -11,8 +15,10 @@ def index():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.html'))
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('/index'))
     return render_template('login.html', title='Sign In', form=form)
